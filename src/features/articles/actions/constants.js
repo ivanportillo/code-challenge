726b260d3a021d9fa890796fb02f3ead5d16bb4f
Iván Portillo Leal <ivanportillo@me.com>
2017-05-16 16:05:39 +0200

    Create actions to article

--- src/features/articles/actions/constants.js
+++ src/features/articles/actions/constants.js
@@ -1,3 +1,7 @@
 export const FETCH_ARTICLES = 'articles/FETCH_ARTICLES';
 export const RECEIVE_ARTICLES = 'articles/RECEIVE_ARTICLES';
 export const RECEIVE_ARTICLES_FAILED = 'articles/RECEIVE_ARTICLES_FAILED';
+
+export const FETCH_ARTICLE = 'articles/FETCH_ARTICLE';
+export const RECEIVE_ARTICLE = 'articles/RECEIVE_ARTICLE';
+export const RECEIVE_ARTICLE_FAILED = 'articles/RECEIVE_ARTICLE_FAILED';
