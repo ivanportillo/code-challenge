a9653a48a79b5e6058528cf7c9cf2ea16ac0b4c5
Iván Portillo Leal <ivanportillo@me.com>
2017-05-16 15:25:36 +0200

    Create ArticlesList container and modify component

---
+++ src/features/articles/containers/ArticlesList.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import { fetchArticles } from '../actions';
+import ArticlesList from '../components/ArticlesList';
+
+const mapStateToProps = state => ({
+  articles: state.articles,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchArticles: () => dispatch(fetchArticles()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ArticlesList);
